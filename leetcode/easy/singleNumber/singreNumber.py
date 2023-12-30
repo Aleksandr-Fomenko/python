4ceb5a7c89f52e9079627e95ffc754b0328c6447
Alexander Fomenko <ffomenko25@gmail.com>
2023-12-30 21:44:57 +0300

    Add leedcode case

---
+++ leetcode/easy/singleNumber/singreNumber.py
@@ -0,0 +1,13 @@
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        return Counter(nums).most_common()[-1][0]
+
+
+if __name__ == '__main__':
+    print(Solution().singleNumber([2, 2, 1]))
+    print(Solution().singleNumber([4, 1, 2, 1, 2]))
+    print(Solution().singleNumber([1]))
